9ee67828ac008501c455b903aec4afa8f4be7e3a
Alex Boag-Munroe <alex.munroe@zpg.co.uk>
2018-10-29 13:20:25 +0000

    Guessing Game v1.0

--- guessing_game.py
+++ guessing_game.py
@@ -1,37 +1,82 @@
 """
 Python Web Development Techdegree
 Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
 """
 
 import random
 
 
+def display_banner():
+    banner = """--------------------------------------
+                |Welcome to the Number Guessing Game!|
+                --------------------------------------
+                |  Press CTRL+C to quit at any time. |
+                --------------------------------------"""
+    return "\n".join(banner_line.lstrip() for banner_line in banner.splitlines())
+
+
+def get_player_guess():
+    try:
+        player_guess = int(input("Pick a number between 1 and 10: "))
+        if player_guess > 10 or player_guess < 1:
+            raise ValueError
+        return player_guess
+    except ValueError:
+        print("Input error, please enter an integer from 1 to 10")
+        return get_player_guess()
+
+
+def check_guess(correct_number, player_guess):
+    if player_guess > correct_number:
+        print("It is lower!")
+        return False
+
+    if player_guess < correct_number:
+        print("It is higher!")
+        return False
+
+    return correct_number == player_guess
+
+
+def play_again_prompt():
+    play_again = input("Would you like to play again? [y]es/[n]o: ")
+    if play_again.lower().startswith("y"):
+        return True
+    elif play_again.lower().startswith("n"):
+        return False
+    else:
+        print("I did not understand.")
+        return play_again_prompt()
+
+
+def end_game(high_score):
+    print("\nThe best score you achieved was {}.".format(high_score))
+    print("Closing the game, see you next time!")
+
+
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
+    print(display_banner())
+    attempts = 0
+    high_score = 0
+    correct_number = random.randint(1, 10)
+    try:
+        while True:
+            player_guess = get_player_guess()
+            attempts += 1
+            if not check_guess(correct_number, player_guess):
+                continue
+            print("You got it! It took you {} tries".format(attempts))
+            if not high_score or attempts < high_score:
+                high_score = attempts
+            if not play_again_prompt():
+                end_game(high_score)
+                break
+            print("The HIGHSCORE is {}".format(high_score))
+            attempts = 0
+    except KeyboardInterrupt:
+        end_game(high_score)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Kick off the program by calling the start_game function.
     start_game()
